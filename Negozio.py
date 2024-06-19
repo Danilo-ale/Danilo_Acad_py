0f240ec92e844ed51a20fa3b3ffe23542573aea5
Danilo-ale <danilo09.alessi@gmail.com>
2024-06-19 16:50:31 +0200

    Add files via upload

--- Negozio.py
+++ Negozio.py
@@ -16,15 +16,23 @@ if(sel.lower()=="si"):
                 print(lista_prod)
                 sel_acq=input("Cosa vuoi acquistare? ")
                 if sel_acq.lower() in lista_prod:
-                    print("Prezzo di", sel_acq,":", lista_prez[lista_prod.index("papera")])
+                    print("Prezzo di", sel_acq,":", lista_prez[lista_prod.index(sel_acq)])
+                    dec_acq=input("Vuoi acquistare? ")
+                    if dec_acq=="si":
+                        print("Prodotto acquistato")
+                        lista_quant[lista_prod.index(sel_acq)]-=1
+                        acquisti_cliente.append=sel_acq
+                        print("Quantita aggiornate:")
+                        print(lista_quant)
+                        print("Acquisti cliente", acquisti_cliente)
+                    else:
+                        pass
                 else:
                     print("Prodotto non presente in lista")
                 cont=input("vuoi continuare? ")
                 if(cont.lower()=="no"):
                     print
                     break   
-
-
 
         elif(scelta.lower()=="no"):
             scelta2=input("Vuoi gestire l'inventario? ")
