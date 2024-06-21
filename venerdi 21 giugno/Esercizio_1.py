8209e4bed4a7533dffad79234d703537154332ae
Danilo-ale <danilo09.alessi@gmail.com>
2024-06-21 14:17:33 +0200

    Esercizio 1 commentato
    Esercizio 1 commentato

--- venerdi 21 giugno/Esercizio_1.py
+++ venerdi 21 giugno/Esercizio_1.py
@@ -1,14 +1,14 @@
 lista=[]
 while True:
-    num=input("Inserisci numero: ")
-    if num.isnumeric():
-        lista.append(num)
+    num=input("Inserisci numero: ")   #
+    if num.isnumeric():         #controllo numero
+        lista.append(num)       #append nella lista
     else:
         print("Scrivi un numero ")
     if(num=="0"):
         break
 sum=0
-for i in lista:
+for i in lista:     #sommo, facendo il cast all'elemento della lista
     sum+=int(i)
 
 print("somma: ",sum)
