afcc30decf6a44b8602b87a82f8aa4a878bdb995
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-01 15:29:43 +0200

    Update punto_cartesiano.py

--- lunedi 1/punto_cartesiano.py
+++ lunedi 1/punto_cartesiano.py
@@ -9,10 +9,10 @@ class Punti:
     def muovi(self, dx,dy):
         self.x=dx
         self.y=dy
-    def distanza_0(self):
+    def distanza_0(self):        #calcolo distanza tra punto e origine
         self.distanza_0=sqrt(self.x**2+self.y**2)
         print("Distanza è:",self.distanza_0)
-    def distanza(self, nuovax,nuovay):
+    def distanza(self, nuovax,nuovay):        #calcolo distanza tra punto originale e un nuovo punto in input
         self.distanza=sqrt((self.x-nuovax)**2+(self.y-nuovay)**2)
         print("Distanza è:",self.distanza)
 punto1=Punti(2,5)
@@ -21,7 +21,7 @@ punto1.muovi(5,4)
 print(punto1.x, punto1.y)
 print("distanza da origine:")
 punto1.distanza_0()
-x,y=input("Inserisci due coordinate: ").split(" ")
+x,y=input("Inserisci due coordinate: ").split(" ")    #prova input
 punto1.distanza(int(x),int(y))
 
 
