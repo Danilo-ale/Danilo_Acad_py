ace18b1c64b449913a697502b55970035924dc8c
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-01 15:36:08 +0200

    Esercizio Classe Libri()

---
+++ lunedi 1/libreria.py
@@ -0,0 +1,22 @@
+class Libri:
+    autore=""
+    titolo=""
+    pagine=""
+    def __init__(self, titolo,autore,pagine):
+        self.autore=autore
+        self.titolo=titolo
+        self.pagine=pagine
+    def __str__(self):
+        print(f"Il libro \"{self.titolo}\" è stato scritto da {self.autore} e ha {self.pagine} pagine")
+    
+
+libro1=Libri("Windows","Bill Gates", 120)
+libro2=Libri("Titolo","Autore","pagine")
+libro1.__str__()
+
+libreria=[]
+libreria.append(libro1)
+libreria.append(libro2)
+
+for libro in libreria:
+    libro.__str__()
