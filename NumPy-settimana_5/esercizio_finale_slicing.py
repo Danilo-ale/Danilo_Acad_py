8b4343044033dbc777cf3fe3aa3a1d56139635c3
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-15 17:50:39 +0200

    Esercizio finale completato e commentato

--- NumPy-settimana_5/esercizio_finale_slicing.py
+++ NumPy-settimana_5/esercizio_finale_slicing.py
@@ -4,6 +4,7 @@ def crea_array():
 
     return arr
 
+#funzioni 
 def stampa_primi_dieci_num():
     print(f"I primi 10 elementi sono: {arr[0:10]}")
 
@@ -13,25 +14,51 @@ def stampa_ultimi_cinque_num():
 def stampa_range():
     print(f"Gli elementi dall'indice 5 all'indice 15 (escluso) sono: {arr[5:15]}")
     
-
 def stampa_step():
     print(f"Elementi con step di 3: {arr[::3]}")
 
 def modifica_elem():
-    arr2=arr.copy()
-    arr2[5:10]=99
+    arr2=arr.copy() #copia nell'array2 gli elementi dell'array 
+    arr2[5:10]=99   #assegna 99 dal quinto al decimo indice
     print(f"L'array modificato è: {arr2}")
 
 def stampa_array_originale():
     print("array originale: ",arr)
 
-
+#creazione array
 arr=crea_array()
 print(arr)
 
-stampa_primi_dieci_num()
-stampa_ultimi_cinque_num()
-stampa_range()
-stampa_step()
-modifica_elem()
-stampa_array_originale()
+#menu
+def menu():
+    print("""\nOPZIONI:
+1. Stampa i primi 10 numeri
+2. Stampa gli ultimi 5 numeri
+3. Stampa numeri dall'indice 5 al 15 escluso
+4. Stampa i numeri con step di 3
+5. Modifica gli elementi dall'indice 5 al 10
+6. Stampa array originale
+0. Esci
+""")
+    
+
+while True:
+    menu()
+    scelta=input("Scegli un'opzione: ")
+    if scelta=="1":     #1. Stampa i primi 10 numeri
+        stampa_primi_dieci_num()
+    elif scelta=="2":   #2. Stampa gli ultimi 5 numeri
+        stampa_ultimi_cinque_num()
+    elif scelta=="3":   #3. Stampa numeri dall'indice 5 al 15 escluso
+        stampa_range()
+    elif scelta=="4":   #4. Stampa i numeri con step di 3
+        stampa_step()
+    elif scelta=="5":   #5. Modifica gli elementi dall'indice 5 al 10
+        modifica_elem()
+    elif scelta=="6":   #6. Stampa array originale
+        stampa_array_originale()
+    elif scelta=="0":   #Esce dal while
+        break
+    else:
+        print("Scelta sbagliata")
+
