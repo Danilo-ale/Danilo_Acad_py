e909534c5f269eb7fe92e20496c88959dbd9cc51
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-16 11:29:20 +0200

    Esercizio finito e commentato

--- NumPy-settimana_5/Martedi 16/es_1_funz_mat.py
+++ NumPy-settimana_5/Martedi 16/es_1_funz_mat.py
@@ -10,18 +10,52 @@ Calcola e stampa la somma degli elementi di entrambe le matrici.
 """
 
 import numpy as np
+#funzione crea array
 def crea_arr():
     arr=np.linspace(0,1,12)
     arr=arr.reshape(3,4)
     print(arr)
     return arr
 
+#funzione crea matrice
 def genera_matrice():
     arr=np.linspace(0,1,12).reshape(3,4)
     print(arr)
     return arr
 
-arr1d=crea_arr()
+#menu
+def menu():
+    print("""1. Crea array
+2. Crea la matrice
+3. Calcola somma array
+4. Calola somma matrice
+0. Esci
+""")
+#variabili di controllo
+contr_arr=False
+contr_mat=False
+while True:
+    menu()
+    scelta=input("Inserisci la scelta: ")
+    if scelta=="1":
+        arr=crea_arr()
+        contr_arr=True  #variabile di controllo. Se è false, non permetterà di sommare i numeri di un array (perché vuoto)
+    elif scelta=="2":
+        mat=genera_matrice()  #variabile di controllo. Se è false, non permetterà di sommare i numeri di una matrice (perché vuota)
+        contr_mat=True
+    elif scelta=="3":
+        if contr_arr:
+            print(f"Somma dell'array: {np.sum(arr)}")
+        else:
+            print("Array non ancora creato")
+    elif scelta=="4":
+        if contr_mat:
+            print(f"Somma della matrice: {np.sum(mat)}")
+        else:
+            print("Matrice non ancora creata")
+    elif scelta=="0":
+        break
+    else:
+        print("Scelta sbagliata")
 
-mat=genera_matrice()
-print(f"Somma dell'array: {np.sum(arr1d)}\nSomma della matrice: {np.sum(mat)}")
+print("Chisura")
