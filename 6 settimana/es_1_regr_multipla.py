bbc0540208c09790ed14d49535e9a750e9e3a038
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-25 15:13:14 +0200

    Add files via upload

--- 6 settimana/es_1_regr_multipla.py
+++ 6 settimana/es_1_regr_multipla.py
@@ -16,22 +16,14 @@ from sklearn.linear_model import LinearRegression
 df=pd.read_csv("6 settimana\\giovedi 25\\Student_Performance.csv")
 
 print(df)
-df.dropna()
 
 df_train=df[["Hours Studied","Sleep Hours"]]
 df_performance=df[["Performance Index"]]
 
-print(df_train)
-print(df_performance)
+X_train, X_test, y_train,y_test=train_test_split(df_train,df_performance, train_size=0.2)
 
 
-X_train, X_test, y_train,y_test=train_test_split(df_train,df_performance)
-
-print(X_test.shape)
-print(y_test.shape)
-
-
-regr=LinearRegression().fit(df_train,df_performance)
+regr=LinearRegression().fit(X_train, y_train)
 #r_sw=coefficiente 
 r_sq=regr.score(X_train, y_train)
 
@@ -41,10 +33,11 @@ print(f"intercept: {regr.intercept_}")
 
 y_pred = regr.predict(X_test)
 print(f"coefficients: {regr.coef_}")
-plt.subplot(2,1,1)
+plt.subplot(2,1,1).set_title("Relazione ore di studio-indice di prestazione")
 plt.scatter(X_test["Hours Studied"],y_test, color="blue")
 plt.plot(X_test["Hours Studied"],y_pred, color="green")
-plt.subplot(2,1,2)
+
+plt.subplot(2,1,2).set_title("Relazione ore di sonno-indice di prestazione")
 plt.scatter(X_test["Sleep Hours"],y_test, color="blue")
 plt.plot(X_test["Sleep Hours"],y_pred, color="green")
 plt.show()
